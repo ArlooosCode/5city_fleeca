67f59d2b9f56f52726b5912312c98886546328b5
K4R00L <99073785+K4R00L@users.noreply.github.com>
2022-02-06 12:57:14 +0100

    Add files via upload

---
+++ items.sql
@@ -0,0 +1,3 @@
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES -- chech if you 
+    ('id_card_f', 'Karta Dostępu', 1, 3, 1),
+    ('secure_card', 'Karta Bezpieczeństwa', 1, 3, 1);
